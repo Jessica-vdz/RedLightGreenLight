78e01eaebce3442aada0192f480ced4cbf8b7dfd
Stanley Dam <ditisstanley@gmail.com>
2024-05-17 22:08:37 +0200

    added behavior for RedLight

--- src/main/java/nl/jessicavdz/redlightgreenlight/RedLightGreenLight.java
+++ src/main/java/nl/jessicavdz/redlightgreenlight/RedLightGreenLight.java
@@ -1,6 +1,7 @@
 package nl.jessicavdz.redlightgreenlight;
 
 import nl.jessicavdz.redlightgreenlight.Block.BlockBreakCancel;
+import nl.jessicavdz.redlightgreenlight.RedLight.RedLightBehavior;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public final class RedLightGreenLight extends JavaPlugin {
@@ -8,7 +9,12 @@ public final class RedLightGreenLight extends JavaPlugin {
     @Override
     public void onEnable() {
         // Plugin startup logic
+        RedLightBehavior redLightBehavior = new RedLightBehavior();
+
         getServer().getPluginManager().registerEvents(new BlockBreakCancel(),this);
+        getServer().getPluginManager().registerEvents(redLightBehavior, this);
+        redLightBehavior.light = false;
+
     }
 }
 
