8bd834dfd94d2a83b85a6396020e0b40d734b0d7
Stanley-Dam <42673142+Stanley-Dam@users.noreply.github.com>
2024-05-17 22:22:41 +0200

    Revert "Feature/redlight behavior"

--- src/main/java/nl/jessicavdz/redlightgreenlight/RedLightGreenLight.java
+++ src/main/java/nl/jessicavdz/redlightgreenlight/RedLightGreenLight.java
@@ -1,7 +1,5 @@
 package nl.jessicavdz.redlightgreenlight;
 
-import nl.jessicavdz.redlightgreenlight.Block.BlockBreakCancel;
-import nl.jessicavdz.redlightgreenlight.RedLight.RedLightBehavior;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public final class RedLightGreenLight extends JavaPlugin {
@@ -9,10 +7,11 @@ public final class RedLightGreenLight extends JavaPlugin {
     @Override
     public void onEnable() {
         // Plugin startup logic
-        RedLightBehavior redLightBehavior = new RedLightBehavior();
 
-        getServer().getPluginManager().registerEvents(new BlockBreakCancel(),this);
-        getServer().getPluginManager().registerEvents(redLightBehavior, this);
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
     }
 }
-
