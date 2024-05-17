194bf7cc2ecc5c485f01a58a3053fc508f37b840
Stanley Dam <ditisstanley@gmail.com>
2024-05-17 21:10:33 +0200

    i added the if player is op so i can still customize the map if i want

--- src/main/java/nl/jessicavdz/redlightgreenlight/Block/BlockBreakCancel.java
+++ src/main/java/nl/jessicavdz/redlightgreenlight/Block/BlockBreakCancel.java
@@ -1,5 +1,6 @@
 package nl.jessicavdz.redlightgreenlight.Block;
 
+import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.block.BlockBreakEvent;
@@ -7,7 +8,10 @@ import org.bukkit.event.block.BlockBreakEvent;
 public class BlockBreakCancel implements Listener {
 
     @EventHandler
+
     public void onBlockBreak(BlockBreakEvent event) {
+        Player player = event.getPlayer();
+        if(player.isOp()) return;
         event.setCancelled(true);
     }
 }
