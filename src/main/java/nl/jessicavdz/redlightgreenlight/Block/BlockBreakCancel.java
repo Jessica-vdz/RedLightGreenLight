d7476f23ed4f32362353360b82c3a0230324756f
Stanley Dam <ditisstanley@gmail.com>
2024-05-17 19:31:27 +0200

    canceled block break even

--- src/main/java/nl/jessicavdz/redlightgreenlight/Block/BlockBreakCancel.java
+++ src/main/java/nl/jessicavdz/redlightgreenlight/Block/BlockBreakCancel.java
@@ -4,9 +4,7 @@ import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.block.BlockBreakEvent;
 
-import java.util.EventListener;
-
-public class BlockBreakCancel implements EventListener, Listener {
+public class BlockBreakCancel implements Listener {
 
     @EventHandler
     public void onBlockBreak(BlockBreakEvent event) {
